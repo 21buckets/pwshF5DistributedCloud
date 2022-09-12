4358501471dcfd698ba0dc91a47d26b129facd33
21buckets <39548246+21buckets@users.noreply.github.com>
2022-09-12 18:58:22 +1000

    Update functions.ps1
    * Adding 'XC' prefix
    * changing to approved PowerShell verbs
    * adding (incomplete) new-XCDNSXone function

--- functions.ps1
+++ functions.ps1
@@ -39,7 +39,7 @@ function Get-XCNamespaces {
 
 
 
-function Get-HttpLoadbalancers {
+function Get-XCHttpLoadbalancers {
     param(
         [Parameter(Mandatory = $true)]
         $namespace
@@ -62,7 +62,7 @@ function Get-HttpLoadbalancers {
     return Invoke-RestMethod @req
 }
 
-function Get-HttpLoadbalancer {
+function Get-XCHttpLoadbalancer {
     param(
         [Parameter(Mandatory = $true)]
         $namespace,
@@ -88,7 +88,7 @@ function Get-HttpLoadbalancer {
 }
 
 
-function Get-Routes {
+function Get-XCRoutes {
     param(
         [Parameter(Mandatory = $true)]
         $namespace
@@ -113,7 +113,7 @@ function Get-Routes {
 }
 
 
-function Get-DNSDomains {
+function Get-XCDNSDomains {
     
     $uri_path = "/api/config/namespaces/system/dns_domains"
     $xc_connection = $global:XCConnection
@@ -131,7 +131,7 @@ function Get-DNSDomains {
     return Invoke-RestMethod @req
 }
 
-function Get-DNSDomain {
+function Get-XCDNSDomain {
 
     param(
         [Parameter(Mandatory=$true)]
@@ -161,7 +161,7 @@ function Get-DNSDomain {
 
 
 
-function Create-DNSDomain {
+function New-XCDNSDomain {
 
     param(
         [Parameter(Mandatory=$true)]
@@ -216,3 +216,220 @@ function Create-DNSDomain {
     return Invoke-RestMethod @req -debug
 }
 
+
+function Start-XCDNSDomainVerify {
+
+    param(
+        [Parameter(Mandatory=$true)]
+        $name,
+
+        [Parameter(Mandatory=$false)]
+        $namespace = 'system'
+
+
+    )
+    
+    $uri_path = "/api/config/namespaces/${namespace}/dns_domain/${name}/verify"
+    $xc_connection = $global:XCConnection
+
+    $req = @{
+        Uri         = $xc_connection.url + $uri_path
+        Method      = 'POST'      
+        ContentType = 'application/json'
+        Headers = @{
+            "Authorization" = "APIToken $($xc_connection.api_token)"
+        }
+    }
+
+
+    return Invoke-RestMethod @req
+}
+
+
+
+function Get-XCDNSZones {
+    param(
+        [Parameter(Mandatory=$false)]
+        $namespace = 'system'
+    )
+
+
+    $uri_path = "/api/config/dns/namespaces/${namespace}/dns_zones"
+    $xc_connection = $global:XCConnection
+
+    $req = @{
+        Uri         = $xc_connection.url + $uri_path
+        Method      = 'GET'      
+        ContentType = 'application/json'
+        Headers = @{
+            "Authorization" = "APIToken $($xc_connection.api_token)"
+        }
+    }
+
+
+    return Invoke-RestMethod @req
+
+}
+
+function Get-XCDNSZone {
+    param(
+        [Parameter(Mandatory=$true)]
+        $name,
+        [Parameter(Mandatory=$false)]
+        $namespace = 'system'
+    )
+
+
+    $uri_path = "/api/config/dns/namespaces/${namespace}/dns_zones/${name}"
+    $xc_connection = $global:XCConnection
+
+    $req = @{
+        Uri         = $xc_connection.url + $uri_path
+        Method      = 'GET'      
+        ContentType = 'application/json'
+        Headers = @{
+            "Authorization" = "APIToken $($xc_connection.api_token)"
+        }
+    }
+
+
+    return Invoke-RestMethod @req
+
+}
+
+
+function Remove-XCDNSZone {
+    param(
+        [Parameter(Mandatory=$true)]
+        $name,
+        [Parameter(Mandatory=$false)]
+        $namespace = 'system'
+    )
+
+
+    $uri_path = "/api/config/dns/namespaces/${namespace}/dns_zones/${name}"
+    $xc_connection = $global:XCConnection
+
+    $req = @{
+        Uri         = $xc_connection.url + $uri_path
+        Method      = 'DELETE'      
+        ContentType = 'application/json'
+        Headers = @{
+            "Authorization" = "APIToken $($xc_connection.api_token)"
+        }
+    }
+
+
+    return Invoke-RestMethod @req
+
+}
+
+
+function New-XCDNSZone {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$name,
+
+        [Parameter(Mandatory=$false)]
+        [string]$description,
+
+        [Parameter(Mandatory=$false)]
+        [PSCustomObject]$annotations,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$disabled,
+
+        [Parameter(Mandatory=$false)]
+        [PSCustomObject]$labels,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [string]$admin,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [switch]$dnssecEnabled,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [switch]$useCustomSOAParameters,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [PSCustomObject]$customSOAParameters,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [Array]$defaultRRSetGroup,
+
+        [Parameter(Mandatory=$false,
+            ParameterSetName='primary')]
+        [Array]$RRSetGroup,
+
+        [Parameter(Mandatory=$false)]
+        $namespace = 'system'
+    )
+
+  
+
+    
+    $uri_path = "/api/config/dns/namespaces/${namespace}/dns_zones"
+    $xc_connection = $global:XCConnection
+
+    
+
+
+    [PSCustomObject]$body = @{
+        "metadata" = @{
+            "name" = $name
+            "description" = $description
+            "annotations" = $annotations
+            "disable" = $disable
+            "labels" = $labels
+            "namespace" = $namespace
+        }
+        "spec" = @{
+            "domain" = $domain
+        }
+    }
+
+
+    $parameter_set = $PsCmdlet.ParameterSetName
+    if($parameter_set -eq "primary"){
+        Write-host "Primary parameter set"
+
+        #Create initial primary spec object
+        $primary_spec = @{
+            "admin" = $admin
+        }
+
+        #if not using custom SOA parameters, use default, otherwise add in custom
+        if($useCustomSOAParameters -eq $false){
+            $primary_spec["default_soa_parameters"] = @{}
+        }else{
+            $primary_spec["soa_parameters"] = $customSOAParameters
+        }
+
+
+
+        $body["primary"] = $primary_spec
+
+    }elseif($parameter_set -eq "secondary"){
+        Write-host "secondary parameter set"
+    }
+
+    ConvertTo-json $body
+
+
+    $req = @{
+        Uri         = $xc_connection.url + $uri_path
+        Method      = 'POST'
+        Body     = $body      
+        ContentType = 'application/json'
+        Headers = @{
+            "Authorization" = "APIToken $($xc_connection.api_token)"
+        }
+    }
+
+   # return Invoke-RestMethod @req -debug
+}
